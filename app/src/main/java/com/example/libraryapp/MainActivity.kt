e5e5aacf3947f775d5ccb5074395180fb56887ae
peterbezanson <bezansonpeter@gmail.com>
2022-06-26 14:48:24 -0400

    Moved book details to a separate fragment.

--- app/src/main/java/com/example/libraryapp/MainActivity.kt
+++ app/src/main/java/com/example/libraryapp/MainActivity.kt
@@ -2,23 +2,11 @@ package com.example.libraryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
-import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import com.example.libraryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var book = Book("MaddAddam", "Margaret Atwood")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        val model: BookDetailViewModel by viewModels()
-        binding.viewModel = model
-
+        setContentView(R.layout.activity_main)
     }
 }
