ce22cc5371b1f16500bd140f93d94a18c989338a
peterbezanson <bezansonpeter@gmail.com>
2022-09-12 11:13:24 -0400

    Add missing InsertBookFragment

---
+++ app/src/main/java/com/example/libraryapp/InsertBookFragment.kt
@@ -0,0 +1,34 @@
+package com.example.libraryapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.Navigation
+
+class InsertBookFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_insert_book, container, false)
+
+        val main_activity : MainActivity = getActivity() as MainActivity
+
+        val button: Button = view.findViewById(R.id.buttonCreateBook)
+        val text: EditText = view.findViewById(R.id.txtTitle)
+        button.setOnClickListener() {
+            main_activity.rep.addBook(Book(text.getText().toString(), "author"))
+            Navigation.findNavController(view).navigate(R.id.action_create_book)
+        }
+        return view
+    }
+
+    companion object {
+
+    }
+}
